804e81d4c9030ab66fdad5b5c633433fa07dea0f
Mafer Angulo <maferadr@Mafers-Air.lan>
2024-01-09 12:09:24 -0500

    Logo.svg is able to be created

--- index.js
+++ index.js
@@ -1,6 +1,77 @@
 //We call for the main file in which the user is going to generate de SVG.
-const userInput = require('./lib/userInput');
+const { writeFile } = require('fs/promises');
+const inquirer = require('inquirer');
+const { Shape }= require('./lib/shapes')
 
-const svgGenerator = new userInput();
+class SVG{
+    constructor(){
+        this.textElement = ''
+        this.shapeElement = ''
+    }
+    render(){
+        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
+    }
+    setTextElement(text, color){
+        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
+    }
+    setShapeElement(shape){
+        this.shapeElement = Shape;
+    }
+}
 
-svgGenerator.run();
+const lenghtValidator = async (input) =>{
+    if(input.length > 0 && input.length < 4){
+        return true
+    }else{
+        return 'Enter a Valid text'
+    }
+}
+
+const userInput = 
+    [
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter the Text Content of your logo.',
+            validate: lenghtValidator
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'Color of preference.'
+        },
+        {
+            type: 'checkbox',
+            name: 'shape',
+            message: 'Choose the shape of your preference.',
+            choices: [
+                'Circle',
+                'Triangle',
+                'Square'
+            ]
+        },
+        {
+            type: 'input',
+            name: 'colorShape',
+            message: 'Enter the color of your preference.'
+        }
+    ]
+
+    const shapeSVG = new SVG();
+    shapeSVG.setTextElement(userInput.text, userInput.color);
+    shapeSVG.setShapeElement(userInput.shape);
+
+
+    inquirer.prompt(userInput)
+    .then((answers)=>{
+        console.log(answers)
+    })
+    .then(()=>{
+        return writeFile('./examples/logo.svg', shapeSVG.render())
+    })
+
+
+
+// const shapeSVG = new Shape();
+// shapeSVG.setTextElement(userInput.text, userInput.color);
+// shapeSVG.setShapeElement(userInput.shape)  
